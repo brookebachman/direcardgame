64cc1603824343967a87f1fc63f01b75e8cb499d
brookebachman00 <bbachman1994@gmail.com>
2020-08-16 16:04:50 -0600

    Game works in calculating total

--- src/index.js
+++ src/index.js
@@ -12,12 +12,22 @@ const suits = {
 	SPADES: 4,
 };
 
+let input = document.getElementById("number")
+
+
 function numPlayers(myDeck) {
 	//while loop while arrays are < 5 because we only want 5 cards per hand
 	//loop over the cards array to load the cards
 	//console.log(myDeck);
+	let playerNum;
+	var nums = /^\d+$/
 	let allHands = {};
-	let playerNum = Math.floor(Math.random() * 6) + 2;
+	if (input.value && nums.test(input.value)) {
+		 playerNum = parseInt(input.value)
+	} else {
+		console.log("chose random")
+	
+ playerNum = Math.floor(Math.random() * 6) + 2; }
 	//console.log(playerNum, 'playernum');
 	for (let i = 1; i <= playerNum; i++) {
 		let hand = [];
@@ -26,55 +36,59 @@ function numPlayers(myDeck) {
 			hand.push(myDeck.shift());
 
 			sortHand(hand);
+			
 		}
-		picturesOfHands(allHands);
+		
 		allHands['Player ' + i] = hand;
 	}
+	picturesOfHands(allHands);
 	//console.log(allHands, 'all hands');
-
+	winner(allHands)
 	//using .shift takes away the item in an array and allows me to take cards out of the remaining deck so that each card is unique and there are no repeats
-
-	winner(allHands);
 }
+	
+
 
 function winner(allHands) {
+	console.log("winner is being called")
 	let winner = 0;
-	let current = 0;
+	let winnerName = ""
+
 	//now need to iterate over the hands and find the total of the value
 	//I keep a current value and if the number I am iterating over
 	//compare the values for the hands
 	//Math.max chooses the larger number of the 2 you give it
 	//return the winners
-	for (let i = 0; i < allHands.length; i++) {
-		if (totalValues(allHands[i]) > totalValues(allHands[i + 1])) {
-			current = totalValues(allHands[i]);
-		} else {
-			winner = Math.max(current, winner);
+	for (const [key, hand] of Object.entries(allHands)){
+
+		if (totalValues(hand) > winner) {
+			winner =  totalValues(hand)
+			winnerName = key
+		
 		}
+
+
 	}
 
-	//console.log(Math.max(current, winner));
+console.log(winner, "winner value", winnerName)
+	//return 
 }
 
 function totalValues(hand) {
 	let total = 0;
-	let values = [];
+
 	//iterate over the array of hashes which
 	for (let i = 0; i < hand.length; i++) {
-		for (const [key, value] of Object.entries(hand[i])) {
-			//total += value
-			values.push(value);
-		}
+		total = total + hand[i].value
 	}
-
 	//i know the number values are at the beginning of the array
 	//i am going to iterate over the array and skip 2 everytime and add those numbs to the sum
 	//then return the sum
-	for (let i = 0; i < values.length; i++) {
-		total += values[i];
-		i = i + 2;
-	}
-	return total;
+	// for (let i = 0; i < values.length; i++) {
+	// 	total += values[i];
+	// 	i = i + 2;
+	// }
+	return total
 }
 
 function card(value, name, suit) {
@@ -128,40 +142,32 @@ function sortHand(hand) {
 	}
 }
 function picturesOfHands(allHands) {
-	let yourHand = document.createElement('h2');
-	let hand1 = document.getElementById('hand');
-	hand1.className = 'text';
-	hand1.appendChild(yourHand);
+	let players = document.getElementById('all-players');
+	players.className = 'text';
 	//here I want to add text so the player knows it is their hand, I need to do it outside of the for loop because that will address every card
 	//console.log(allHands)
-	for (const [key, values] of Object.entries(allHands)) {
-		//console.log(`${key}: ${value}`);
-		yourHand.innerHTML = key;
-		let card = document.createElement('div');
-		//here we are iterating over one hand
-		//console.log(key)
+	for (const [playerNumber1, hand] of Object.entries(allHands)) {
+		let playerNumber = document.createElement('h2');
+		playerNumber.innerHTML = playerNumber1;
+		players.appendChild(playerNumber);
 
-		for (var i = 0; i < values.length; i++) {
-			console.log(typeof values[i]);
-			for (const [key2, value2] of Object.entries(values[i])) {
-				console.log(`${key2}`);
+		for (var i = 0; i < hand.length; i++) {
+			let card = document.createElement('div');
+			console.log(hand[i].value);
+			let currentCard = hand[i]
 				card.className = 'card';
-				if (key2.suit == 2) {
+				if (hand[i].suit == 2) {
 					var ascii_char = '♦';
-				} else if (key2.suit == 3) {
+				} else if (hand[i].suit == 3) {
 					var ascii_char = '♣';
-				} else if (key2.suit == 4) {
+				} else if (hand[i].suit == 4) {
 					var ascii_char = '♠';
 				} else {
 					var ascii_char = '♥';
 				}
-				for (let i = 0; i < key2.length; i++) {
-					console.log(key2[i].name);
-					card.innerHTML = '' + key2[i].name + '' + ascii_char + '';
-				}
-
-				hand1.appendChild(card);
-			}
+				card.innerHTML = '' + hand[i].name + '' + ascii_char + '';
+				players.appendChild(card);
+			
 		}
 	}
 }
